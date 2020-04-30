5d9fb72a8969005a1c35cba5c6c657c246900e1c
sncandiani <sofiancandiani@gmail.com>
2020-04-30 09:50:53 -0500

    changed the main page, moving over code

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from cohort import Cohort
 from exercise import Exercise 
 from instructor import Instructor 
 from student import Student
-from nssperson import NSSPerson
 
 daily_journal = Exercise("Daily Journal", "Javascript")
 student_exercises = Exercise("Student Exercises", "Python")
@@ -14,19 +13,38 @@ cohort_37 = Cohort("Cohort 37")
 cohort_24 = Cohort("Cohort 24")
 cohort_9 = Cohort("Evening Cohort 9")
 
-bjork_g = NSSPerson("Bjork", "Guðmundsdóttir", "@bjorkg", cohort_9)
-kate_bush = NSSPerson("Kate", "Bush", "@wutheringheights", cohort_38)
-kurt_cobain = NSSPerson("Kurt", "Cobain", "@nirvana", cohort_37)
-billy_holiday = NSSPerson("Billy", "Holiday", "@gloomybilly", cohort_24)
+bjork_g = Student("Bjork", "Guðmundsdóttir", "@bjorkg", cohort_9)
+kate_bush = Student("Kate", "Bush", "@wutheringheights", cohort_38)
+kurt_cobain = Student("Kurt", "Cobain", "@nirvana", cohort_37)
+billy_holiday = Student("Billy", "Holiday", "@gloomybilly", cohort_24)
 
-thom_yorke = NSSPerson("Thom", "Yorke", "@thomyorke", cohort_9)
-elizabeth_fraiser = NSSPerson("Elizabeth", "Frasier", "@pearlydewdropsdrop", cohort_38)
-billy_corgan = NSSPerson("Billy", "Corgan", "@patrick", cohort_24)
-bilinda_butcher = NSSPerson("Bilinda", "Butcher", "@bilindab", cohort_37)
+thom_yorke = Instructor("Thom", "Yorke", "@thomyorke", cohort_9, "Singing solemnly")
+elizabeth_fraiser = Instructor("Elizabeth", "Frasier", "@pearlydewdropsdrop", cohort_38, "Yelling nonsense that is vaguely profound")
+billy_corgan = Instructor("Billy", "Corgan", "@patrick", cohort_24, "Wailing")
+bilinda_butcher = Instructor("Bilinda", "Butcher", "@bilindab", cohort_37, "A stare which reveals all")
 
-#How can I add special traits?
+bilinda_butcher.assign_exercise(kurt_cobain, perennial)
+billy_corgan.assign_exercise(billy_holiday, nutshell)
+elizabeth_fraiser.assign_exercise(kate_bush, student_exercises)
+thom_yorke.assign_exercise(bjork_g, daily_journal)
 
-# bilinda_butcher.assign_exercise(kurt_cobain, perennial)
-# billy_corgan.assign_exercise(billy_holiday, nutshell)
-# elizabeth_fraiser.assign_exercise(kate_bush, student_exercises)
-# thom_yorke.assign_exercise(bjork_g, daily_journal)
+students = list()
+instructors = list()
+def addStudents(student): 
+    students.append(student.first_name)
+def addInstructors(instructor): 
+    instructors.append(instructor.first_name)
+
+addStudents(bjork_g)
+addStudents(kate_bush)
+addStudents(kurt_cobain)
+addStudents(billy_holiday)
+
+addInstructors(thom_yorke)
+addInstructors(billy_corgan)
+addInstructors(elizabeth_fraiser)
+addInstructors(bilinda_butcher)
+
+print(students)
+print(instructors)
+
